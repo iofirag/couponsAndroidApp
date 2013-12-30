c91f5921602f59821282f80f32f6b9c9df0b5f74
ofir agahi <iofirag@gmail.com>
2013-12-30 15:41:15 +0200

    first commit

---
+++ app/src/main/java/com/oa/couponswebapp/MainActivity.java
@@ -0,0 +1,104 @@
+package com.oa.couponswebapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    Context context=this;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+
+        if (!DetectConnection.checkInternetConnection(this)) {
+            Toast.makeText(getApplicationContext(), "No Internet!", Toast.LENGTH_SHORT).show();
+        } else {
+
+            WebView wv = (WebView) findViewById(R.id.webView);
+            WebViewClient c = new WebViewClient();
+            if (c!=null){
+                wv.setWebViewClient(c);
+            }
+            wv.clearCache(true);
+            wv.clearHistory();
+            wv.getSettings().setJavaScriptEnabled(true);
+            wv.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            wv.getSettings().setBuiltInZoomControls(true);
+            wv.loadUrl("http://couponswebapp.vandervidi.cloudbees.net/views/");
+        }
+    }
+
+    // Function to load all URLs in same webview
+    private class CustomWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+
+            if (!DetectConnection.checkInternetConnection(context)) {
+                Toast.makeText(getApplicationContext(), "No Internet!", Toast.LENGTH_SHORT).show();
+            } else {
+                view.loadUrl(url);
+            }
+
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
